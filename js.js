ac9e68ff0e5cf4221ad0fb341904dcbca90be1b4
Jpc11190 <91492692+Jpc11190@users.noreply.github.com>
2022-11-09 22:13:06 -0500

    Add files via upload

---
+++ js.js
@@ -0,0 +1,47 @@
+
+
+
+
+
+function openMenu(){
+        document.getElementById('menuCon').style.width='250px';
+    
+}
+
+function closeMenu(){
+    document.getElementById('menuCon').style.width='0px';
+
+} 
+
+
+
+var countDownDate = new Date("Nov 18, 2022 15:37:25").getTime();
+
+// Update the count down every 1 second
+var x = setInterval(function() {
+
+  // Get today's date and time
+  var now = new Date().getTime();
+    
+  // Find the distance between now and the count down date
+  var distance = countDownDate - now;
+    
+  // Time calculations for days, hours, minutes and seconds
+  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+    
+  // Output the result in an element with id="demo"
+  document.getElementById('dSet').innerHTML= days;
+  document.getElementById('hSet').innerHTML = hours;
+  document.getElementById('mSet').innerHTML = minutes;
+  document.getElementById('sSet').innerHTML = seconds;
+  
+    
+  // If the count down is over, write some text 
+  if (distance < 0) {
+    clearInterval(x);
+    document.getElementById("demo").innerHTML = "EXPIRED";
+  }
+}, 1000);
